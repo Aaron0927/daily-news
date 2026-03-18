e5f6be1228070940093e1f1bfb90208023e03f99
Aaron <aaronxiao0927@gmail.com>
2026-03-18 23:58:10 +0800

    feat add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,44 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
+import {Toaster} from "@/components/ui/sonner"
+import {ThemeProvider} from "@/components/ui/theme-provider"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Daily News",
-  description: "Daily News",
+    title: "Daily News",
+    description: "Daily News",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Toaster position="top-center" />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+            <Toaster position="top-center"/>
+        </ThemeProvider>
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { BookOpenCheck } from 'lucide-react';
 import Card from "@/components/Card";
 import {useState} from "react";
 import { toast } from "sonner";
+import {ModeToggle} from "@/components/ModeToggle";
 
 export default function Home() {
     const [email, setEmail] = useState('');
@@ -28,16 +29,17 @@ export default function Home() {
     };
 
     return (
-        <div className="min-h-screen bg-white">
+        <div className="min-h-screen bg-white dark:bg-zinc-900">
             {/*header*/}
             <header className="flex items-center justify-between px-8 py-6 max-w-7xl mx-auto">
                 <div className="flex items-center gap-3">
-                    <BookOpenCheck className="text-black" />
+                    <BookOpenCheck className="text-black dark:text-white" />
                     <h1 className="text-2xl font-bold">Daily News</h1>
                 </div>
                 <nav className="flex items-center gap-6">
-                    <a href="/" className="hover:text-gray-500">About</a>
-                    <a href="/" className="hover:text-gray-500">Contact</a>
+                    <a href="#" className="hover:text-gray-500">About</a>
+                    <a href="#" className="hover:text-gray-500">Contact</a>
+                    <ModeToggle />
                 </nav>
             </header>
 
@@ -46,15 +48,15 @@ export default function Home() {
                 {/*title*/}
                 <div className="text-center mb-16">
                     <h2 className="text-5xl font-bold mb-6">Daily Brief of AI</h2>
-                    <p className="text-lg text-gray-700 max-w-3xl mx-auto leading-relaxed">
+                    <p className="text-lg text-gray-700 dark:text-gray-50 max-w-3xl mx-auto leading-relaxed">
                         Discover the latest news and insights in the world of AI. Stay updated with the latest developments and trends in artificial intelligence.
                     </p>
                 </div>
 
                 {/*input and subscribe button*/}
                 <div className="flex justify-center items-center gap-4">
-                    <input type="email" value={email} onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter your email" className="w-full max-w-md p-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-                    <button onClick={handleSubscribe} className="bg-black text-white px-6 py-3 rounded-lg hover:bg-blue-500 transition-colors">Subscribe</button>
+                    <input type="email" value={email} onChange={(e) => {setEmail(e.target.value)}} placeholder="Enter your email" className="w-full max-w-md p-3 rounded-lg border border-gray-300 dark:border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-950"/>
+                    <button onClick={handleSubscribe} className="bg-blue-600 text-white dark:text-gray-100 px-6 py-3 rounded-lg hover:bg-blue-700 dark:hover:bg-blue-900 transition-colors">Subscribe</button>
                 </div>
 
                 {/*cards*/}
